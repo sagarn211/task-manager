d724a79790ba46fa2776093329e76b2a1228279f
Sagar Nayak <sagarn0249s@gmail.com>
2026-04-23 19:12:40 +0530

    issue fixres

--- Backend/app.js
+++ Backend/app.js
@@ -15,9 +15,8 @@ app.use(cors({
     origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
 }));
-app.options('*', cors());
 app.use(cookieParser());
 app.use(express.json());
 
